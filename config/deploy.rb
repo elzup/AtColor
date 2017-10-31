be5446f372191bd5a1973f1a4e25f67897638a07
elzup <guild0105@gmail.com>
2017-10-31 17:06:46 +0900

    Capistrano db seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -86,3 +86,16 @@ namespace :deploy do
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
 end
+
+namespace :deploy do
+  desc 'db_seed'
+  task :db_seed do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end

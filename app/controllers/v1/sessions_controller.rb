928ab67e7c6f1abed7a36b810cc08cb1f6063beb
elzup <guild0105@gmail.com>
2017-11-13 12:00:00 +0900

    Register logic

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,13 +2,27 @@ module V1
   class SessionsController < ApplicationController
     skip_before_action :authenticate_user_from_token!
 
-    # POST /v1/login
+    # POST /v1/register
     def create
       if User.exists?(username: user_params[:username])
         @user = User.find_for_database_authentication(username: params[:username])
-        unless @user.valid_password?(params[:passwordj])
+        unless @user.valid_password?(params[:password])
           return render json: {error: 'パスワードが一致しません。'}, status: :unprocessable_entity
         end
+      else
+        @user = User.new user_params
+        unless @user.save!
+          return render json: {error: 'ユーザを作成することが出来ませんでした。'}, status: :unprocessable_entity
+        end
+      end
+      sign_in :user, @user
+      render json: @user, serializer: SessionSerializer, root: nil
+    end
+
+    # POST /v1/login
+    def login
+      if User.exists?(username: user_params[:username])
+        return render json: {error: '既に登録されています。'}, status: :unprocessable_entity
       else
         @user = User.new user_params
         unless @user.save!

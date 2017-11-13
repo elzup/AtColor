efd68d7cbb3707bf537756557fcc904ac38ffd40
elzup <guild0105@gmail.com>
2017-11-14 02:19:11 +0900

    Question update

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,7 @@ class Question < ApplicationRecord
   alias solvers users
 
   def self.setup
+    Question.delete_all
     Question.create(qid: 1, title: 'can you GET?', description: '自分のプロフィールを取得する。', point: 2)
     Question.create(qid: 2, title: 'can you POST?', description: 'ユーザを作成する。', point: 1)
     Question.create(qid: 3, title: 'can you PUT?', description: '自分のプロフィールを更新する。', point: 3)
@@ -13,8 +14,8 @@ class Question < ApplicationRecord
     Question.create(qid: 5, title: 'can you set headers?', description: 'GET /q/5', point: 7)
     Question.create(qid: 6, title: 'can you get headers?', description: 'GET /q/6', point: 11)
 
-    Question.create(qid: 7, title: 'can you get next page?', description: 'GET /q/7', point: 13)
-    Question.create(qid: 8, title: 'can you get last page?', description: 'GET /q/8', point: 17)
+    Question.create(qid: 7, title: 'can you paging?', description: 'GET /q/7', point: 13)
+    Question.create(qid: 8, title: 'can you search?', description: 'GET /q/8', point: 17)
 
     Question.create(qid: 9, title: 'can you timestamp?', description: 'POST /q/9', point: 19)
     Question.create(qid: 10, title: 'can you resend?', description: 'POST /q/10', point: 23)

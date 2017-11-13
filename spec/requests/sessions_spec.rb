116169053539b03c233d4131f120219af94b8b23
elzup <guild0105@gmail.com>
2017-11-13 11:33:36 +0900

    add Password test

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -6,5 +6,27 @@ RSpec.describe "Sessions", type: :request do
       post v1_login_index_path, params: { username: 'hoge', password: 'hogefuga' }
       expect(response).to have_http_status(200)
     end
+
+    it "register no password" do
+      post v1_login_index_path, params: { username: 'hoge' }
+      expect(response).to have_http_status(200)
+    end
+
+    it "register no username" do
+      post v1_login_index_path, params: { passwrod: 'hoge' }
+      expect(response).to have_http_status(200)
+    end
+
+    it "no password" do
+      User.create(username: 'fuga', password: 'hogefuga')
+      post v1_login_index_path, params: { username: 'fuga' }
+      expect(response).to have_http_status(422)
+    end
+
+    it "invalid password" do
+      User.create(username: 'fuga', password: 'hogefuga')
+      post v1_login_index_path, params: { username: 'fuga', password: 'a' }
+      expect(response).to have_http_status(422)
+    end
   end
 end

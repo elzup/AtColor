24264af99f0b7d2c7d2ab66b86c4adb74faa765a
elzup <guild0105@gmail.com>
2017-11-14 10:42:38 +0900

    Response point

--- app/serializers/v1/question_serializer.rb
+++ app/serializers/v1/question_serializer.rb
@@ -1,7 +1,7 @@
 module V1
   class QuestionSerializer < ActiveModel::Serializer
 
-    attributes :id, :title, :description
+    attributes :id, :title, :description, :point
     has_many :solvers
   end
 end

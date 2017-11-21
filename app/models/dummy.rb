eb84850c21ed5f9279dbf62254f9223b2da90709
elzup <guild0105@gmail.com>
2017-11-21 17:28:40 +0900

    Fix pi and magic number

--- app/models/dummy.rb
+++ app/models/dummy.rb
@@ -15,7 +15,7 @@ class Dummy < ApplicationRecord
             }
   validates :c,
             presence: true,
-            inclusion: {in: [3.14159215], message: "pi"},
+            inclusion: {in: Math::PI.to_s.slice(0, 10), message: "pi"},
             length: {is: 10}
   validates :d,
             inclusion: {in: [false]},
